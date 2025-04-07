ba009da9fa2e01b35cda4546df46725983155c2e
WilliamMarroush <marroushw@yahoo.com>
2025-04-07 12:14:09 -0400

    Setting up API calls, adding more general states

--- sunSetRiseZip/src/App.jsx
+++ sunSetRiseZip/src/App.jsx
@@ -6,8 +6,38 @@ import './App.css'
 const zipInfoURL = "https://ctp-zip-code-api.onrender.com/zip/";
 const riseSetInfoURL = "https://api.sunrise-sunset.org/json?";
 
+
+
+
 function App() {
   const [count, setCount] = useState(0)
+  const [longLat, setLongLat] = useState({long: "0", lat: "0"});
+  const [zip, setZip] = useState("00000");
+  const [date,setDate] = useState("2023-01-01");
+  const [riseSetData, setRiseSetData] = useState(null);
+
+  function getLongLatData(zip){
+    const response = fetch(zipInfoURL + zip);
+    const data = response.json()[0];
+    setLongLat({long: data.Long, lat: data.Lat});
+  }
+
+  function getRiseSetData(long, lat, date){
+    if (date == null){
+      date = "today";}
+    const respone = fetch(riseSetInfoURL + "lat="+lat+"&lng="+long+"&date="+date);
+    const data = response.json();
+    setRiseSetData([data.sunrise, data.sunset]);
+  }
+
+  function displayRiseSetData(){
+    return(
+      <>
+        <h3>Sunrise: </h3>
+        <h3>Sunset: </h3>
+      </>
+    );
+  }
 
   return (
     <>
